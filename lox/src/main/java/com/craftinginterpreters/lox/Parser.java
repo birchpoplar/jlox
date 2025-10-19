6f44277704388a3331a414941541d7e368bbba80
Johnnie Walker <jw12866@gmail.com>
2025-10-19 07:21:33 -0400

    finished part II !!!!

--- lox/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ lox/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -49,6 +49,12 @@ class Parser {
         ParserMonitor.enterRule("classDeclaration", peek(), current);
         Token name = consume(IDENTIFIER, "Expect class name.");
 
+        Expr.Variable superclass = null;
+        if (match(LESS)) {
+            consume(IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         consume(LEFT_BRACE, "Expect '{' before class body.");
 
         List<Stmt.Function> methods = new ArrayList<>();
@@ -59,7 +65,7 @@ class Parser {
         consume(RIGHT_BRACE, "Expect '}' after class body.");
         ParserMonitor.exitRule("classDeclaration", true);
      
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt statement() {
@@ -392,6 +398,14 @@ class Parser {
         if (match(NUMBER, STRING)) {
             ParserMonitor.exitRule("primary", true);
             return new Expr.Literal(previous().literal);
+        }
+
+        if (match(SUPER)) {
+            Token keyword = previous();
+            consume(DOT, "Expect '.' after 'super'.");
+            Token method = consume(IDENTIFIER, "Expect superclass method name.");
+            ParserMonitor.exitRule("primary", true);
+            return new Expr.Super(keyword, method);
         }
 
         if (match(THIS)) {
